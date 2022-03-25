0df6bb7e6cea0c638ed878cf52ba10f73026ba80
HaDeaHyuk <84432918+HaDeaHyuk@users.noreply.github.com>
2022-03-25 22:29:42 +0900

    JS: Filter Projects

--- main.js
+++ main.js
@@ -54,6 +54,30 @@ arrowUp.addEventListener('click', () => {
     scrollSection('#home');
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter =
+    e.target.dataset.filter || e.target.parenNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) =>{
+            if (filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
+
+// 
 function scrollSection(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
